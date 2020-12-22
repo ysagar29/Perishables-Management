4a62e2e99d24439e2544003f69f2d5c705a17481
nEW u <nEW u@BT1606024>
2020-12-22 21:33:02 +0530

    changes made

---
+++ perishables-management/src/main/java/com/incture/daos/ZvendDao.java
@@ -0,0 +1,9 @@
+package com.incture.daos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.incture.dos.Zvend;
+
+public interface ZvendDao extends JpaRepository<Zvend, String> {
+
+}
